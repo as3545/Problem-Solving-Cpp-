c0b2051ecc1ac1f883c706913be88e3eaffef537
as3545 <86479682+as3545@users.noreply.github.com>
2023-12-21 21:38:21 +0530

    A Long BIg Sum.cpp

--- A Long BIg Sum.cpp
+++ A Long BIg Sum.cpp
@@ -40,3 +40,14 @@ for(int i=0;i<ar.size();i++){
     sum += ar[i];
 } return sum;
 }
+
+//or
+
+
+long aVeryBigSum(vector<long> ar) {
+    int n=ar.size();
+    long long sum=0;
+    for(int i=0;i<n;i++){
+        sum += ar[i];
+    } return sum;
+}
