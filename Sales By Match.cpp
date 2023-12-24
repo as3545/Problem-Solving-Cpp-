38dbb17d7fa51ede504d3a55f1246f18790c3c36
as3545 <86479682+as3545@users.noreply.github.com>
2023-12-24 17:49:27 +0530

    Sales By Match.cpp

--- Sales By Match.cpp
+++ Sales By Match.cpp
@@ -1,5 +1,4 @@
 /*
-
   There is a large pile of socks that must be paired by color. Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
 
 Example
