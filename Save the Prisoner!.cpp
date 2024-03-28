79300dd8aa1294bb9bc2ef302783f5c371faaa43
as3545 <86479682+as3545@users.noreply.github.com>
2024-03-29 03:12:29 +0530

    Save the Prisoner!.cpp

---
+++ Save the Prisoner!.cpp
@@ -0,0 +1,11 @@
+/*
+Calculate the total number of candies to be distributed.
+Determine the starting chair number for candy distribution.
+Calculate the remaining candies after evenly distributing candies to all prisoners except the one who receives the last candy.
+Calculate the position of the prisoner who will receive the last candy.
+*/
+int r=m%n;
+    int l=(s+r-1)%n;
+    if(l==0){
+        l=n;
+    } return l;
